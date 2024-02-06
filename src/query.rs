f8d67a98bd0e0bab095058c27b1c25307eeac39c
st170001 <st170001@stud.uni-stuttgart.de>
2024-02-06 15:13:25 +0100

    Event server from GRPC to HTTP

--- src/query.rs
+++ src/query.rs
@@ -12,7 +12,7 @@ pub struct Query;
 #[Object]
 impl Query {
     /// Entity resolver for user of specific id.
-    #[graphql(entity)]
+    //#[graphql(entity)]
     async fn user_entity_resolver<'a>(
         &self,
         ctx: &Context<'a>,
